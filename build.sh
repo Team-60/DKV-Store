09be7173f1fff8c7c273a66b9efc936f25f493a9
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2021-12-26 21:09:31 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+# Create build directory
+mkdir -p cmake/build
+
+pushd cmake/build
+
+# Current Installation Path
+export MY_INSTALL_DIR=$HOME/.local
+
+cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR ../..
+make
+
+popd
