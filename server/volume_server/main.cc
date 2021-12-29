f9949d9e33775c522ac57cb112f3a816e0778d1b
Divyansh Rastogi <divyansh19464@iiitd.ac.in>
2021-12-29 10:05:31 -0800

    refactor master -> volume_server

--- server/volume_server/main.cc
+++ server/volume_server/main.cc
@@ -1,14 +1,14 @@
-#include "MasterImpl.h"
+#include "volume_server.h"
 
 void RunServer() {
   std::string server_address("127.0.0.1:8080");
 
-  MasterImpl master(1);
+  VolumeServerImpl volume_server(1);
 
   ::grpc::ServerBuilder builder;
   builder.AddListeningPort(server_address, ::grpc::InsecureServerCredentials());
 
-  builder.RegisterService(&master);
+  builder.RegisterService(&volume_server);
   std::unique_ptr<::grpc::Server> server = builder.BuildAndStart();
 
   std::cout << "Listening on : " << server_address << std::endl;
