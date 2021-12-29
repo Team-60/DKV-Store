3d2809ff0ed9441f714b1cf919cceca3441e1b49
Divyansh Rastogi <divyansh19464@iiitd.ac.in>
2021-12-29 10:50:08 -0800

    add rebuild options

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # Create build directory
-rm -rf cmake
+## flag "D" provided -> rebuild
+if [ $# -eq 1 ]; then
+	if [ $1 == "D" ]; then echo "Rebuilding..." && rm -rf cmake; fi
+fi
 mkdir -p cmake/build
 
 pushd cmake/build
