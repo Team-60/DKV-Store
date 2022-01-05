c336847c968964b66f9fb7e382201033b8cb3c22
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2022-01-06 02:29:15 +0530

    fixed simple shardkv tests: passing

--- tests/simple_shardkv_tests/missing_keys.cc
+++ tests/simple_shardkv_tests/missing_keys.cc
@@ -8,39 +8,40 @@
 using namespace std;
 
 int main() {
-  char hostnamebuf[256] = "127.0.0.1";
-  gethostname(hostnamebuf, 256);
-  string hostname(hostnamebuf);
+  std::string hostname = "127.0.0.1";
+
+  string shardmaster_addr = hostname + ":8080";
+  start_shardmaster(shardmaster_addr);
 
   const string skv_1 = hostname + ":8081";
   const string skv_2 = hostname + ":8082";
   const string skv_3 = hostname + ":8083";
 
-  start_simple_shardkvs({skv_1, skv_2, skv_3});
+  //start_simple_shardkvs({skv_1, skv_2, skv_3});
+  start_simple_shardkv(skv_1, 1);
 
   assert(test_get(skv_1, 0, nullopt));
-  assert(test_get(skv_2, 1, nullopt));
-  assert(test_get(skv_3, 2, nullopt));
-
   assert(test_put(skv_1, 0, "cs15", true));
-  assert(test_put(skv_2, 1, "cs17", true));
-  assert(test_put(skv_3, 2, "cs19", true));
-
   assert(test_get(skv_1, 0, "cs15"));
-  assert(test_get(skv_2, 1, "cs17"));
-  assert(test_get(skv_3, 2, "cs19"));
-
-  assert(test_get(skv_1, 30, nullopt));
-  assert(test_get(skv_2, 34, nullopt));
-  assert(test_get(skv_3, 41, nullopt));
-
-  assert(test_append(skv_1, 30, "cs131", true));
-  assert(test_append(skv_2, 34, "cs32", true));
-  assert(test_append(skv_3, 41, "cs33", true));
-
+  assert(test_delete(skv_1, 0, "cs15", true));
+  assert(test_delete(skv_1, 0, "cs15", false));
+  assert(test_put(skv_1, 30, "cs131", true));
   assert(test_get(skv_1, 30, "cs131"));
-  assert(test_get(skv_2, 34, "cs32"));
-  assert(test_get(skv_3, 41, "cs33"));
+
+  assert(test_leave(shardmaster_addr, skv_1, true));
+
+  std::chrono::milliseconds timespan(1000);
+  std::this_thread::sleep_for(timespan);
+  start_simple_shardkv(skv_2, 2);
+
+  assert(test_get(skv_2, 1, nullopt));
+  assert(test_put(skv_2, 1, "cs17", true));
+  assert(test_get(skv_2, 1, "cs17"));
+  assert(test_get(skv_2, 34, nullopt));
+  assert(test_delete(skv_2, 34, "cs32", false));
+  assert(test_get(skv_2, 34, nullopt));
+
+  assert(test_leave(shardmaster_addr, skv_2, true));
 
   return 0;
 }
--- tests/simple_shardkv_tests/simple_all_ops.cc
+++ tests/simple_shardkv_tests/simple_all_ops.cc
@@ -8,34 +8,44 @@
 using namespace std;
 
 int main() {
-  char hostnamebuf[256] = "127.0.0.1";
-  gethostname(hostnamebuf, 256);
-  string hostname(hostnamebuf);
+  std::string hostname = "127.0.0.1";
+  
+  const string shardmaster_addr = hostname + ":8080";
+  const string skv_1 = hostname + ":8081";
+  const string skv_2 = hostname + ":8082";
+  const string skv_3 = hostname + ":8083";
 
-  const string skv_1 = hostname + ":8080";
-  const string skv_2 = hostname + ":8081";
-  const string skv_3 = hostname + ":8082";
-
-  start_simple_shardkvs({skv_1, skv_2, skv_3});
+  start_shardmaster(shardmaster_addr);
+  start_simple_shardkv(skv_1, 1);
 
   assert(test_get(skv_1, 0, nullopt));
-  assert(test_get(skv_2, 1, nullopt));
-  assert(test_get(skv_3, 2, nullopt));
-
-  assert(test_put(skv_3, 2, "abcd", true));
   assert(test_get(skv_1, 2, nullopt));
-  assert(test_get(skv_2, 2, nullopt));
-  assert(test_get(skv_3, 2, "abcd"));
-
   assert(test_put(skv_1, 0, "cs131!", true));
   assert(test_get(skv_1, 0, "cs131!"));
 
-  assert(test_append(skv_2, 1, "huh?", true));
+  assert(test_leave(shardmaster_addr, skv_1, true));
+
+  std::chrono::milliseconds timespan(1000);
+  std::this_thread::sleep_for(timespan);
+  start_simple_shardkv(skv_2, 2);
+
+  assert(test_get(skv_2, 1, nullopt));
+  assert(test_get(skv_2, 2, nullopt));
+  assert(test_put(skv_2, 1, "huh?", true));
   assert(test_get(skv_2, 1, "huh?"));
-  assert(test_put(skv_2, 1, "huh!", true));
-  assert(test_get(skv_2, 1, "huh!"));
-  assert(test_append(skv_2, 1, "?", true));
-  assert(test_get(skv_2, 1, "huh!?"));
+  assert(test_put(skv_2, 1, "huh!", false));
+  assert(test_get(skv_2, 1, "huh?"));
+  assert(test_delete(skv_2, 1, "huh?", true));
+
+  assert(test_leave(shardmaster_addr, skv_2, true));
+  std::this_thread::sleep_for(timespan);
+  start_simple_shardkv(skv_3, 3);
+
+  assert(test_get(skv_3, 2, nullopt));
+  assert(test_put(skv_3, 2, "abcd", true));
+  assert(test_get(skv_3, 2, "abcd"));
+
+  assert(test_leave(shardmaster_addr, skv_3, true));
 
   return 0;
 }
